09d087b735cb69ce71a04299d89d3f00e714e178
francesco <info@francescovitale.com>
2015-02-12 15:39:09 +0100

    exampleView testing (doesn't work)

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
-// initialise selection tooggle
-$(document).ready(function() {
-    $('select').material_select();
- });
+
  
 // MVC
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
-	
+
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"));
+	var exampleView = new ExampleView($("#exampleView"), model);
 });
+
+// initialise selection tooggle
+$(document).ready(function() {
+    $('select').material_select();
+ });
 
 
---
+++ js/view/exampleViewController.js
@@ -0,0 +1,8 @@
+//ExampleView Object constructor
+var ExampleView = function (container, model) {
+	
+	model.setNumberOfGuests(3);
+
+}
+
+
