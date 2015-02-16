8f8c70663a28e9559ed40b8bc866de6d1af7c0ce
francesco <info@francescovitale.com>
2015-02-16 21:03:44 +0100

    top bar with number of guests in confirmDinnerView and recipeView

--- js/view/recipeView.js
+++ js/view/recipeView.js
@@ -7,11 +7,48 @@ var RecipeView = function (container, model) {
 	this.recipe = container.find("#recipeView");
 
 	// set data to test view
+	model.setNumberOfGuests(2);
 	model.addDishToMenu(2);
 	model.addDishToMenu(103);
 	model.addDishToMenu(200);
 	var menu = model.getFullMenu();
+	var guests = model.getNumberOfGuests();
 
+	// top bar
+	var topBar = document.createElement('div');
+	topBar.setAttribute("id", "top-bar");
+	topBar.setAttribute("class", "row amber lighten-3");
+
+	// My Dinner with number of guests
+	var dinnerLeft = document.createElement('div');
+	dinnerLeft.setAttribute("class", "col s12 m4 l4");
+	var dinnerHeading = document.createElement('h3');
+	dinnerHeading.setAttribute("class", "small-heading");
+	var dinnerHeadingText = document.createTextNode("My Dinner: " + guests + " people");
+	dinnerHeading.appendChild(dinnerHeadingText);
+	dinnerLeft.appendChild(dinnerHeading);
+
+	// back button
+	var dinnerRight = document.createElement('div');
+	dinnerRight.setAttribute("class", "col s12 m4 l4 offset-m4 offset-l4");
+
+	var buttonContainer = document.createElement('p');
+	var buttonLink = document.createElement('a');
+	buttonLink.setAttribute("class", "waves-effect waves-light btn");
+	var buttonIcon = document.createElement('i');
+	buttonIcon.setAttribute("class", "mdi-navigation-arrow-back left");
+	var buttonText = document.createTextNode("Go back and edit dinner");
+	buttonLink.appendChild(buttonIcon);
+	buttonLink.appendChild(buttonText);
+	buttonContainer.appendChild(buttonLink);
+	dinnerRight.appendChild(buttonContainer);
+
+	topBar.appendChild(dinnerLeft);
+	topBar.appendChild(dinnerRight);
+	document.getElementById("recipeView").appendChild(topBar);
+
+
+	// dishes
 	var dishes = document.createElement('div');
 	dishes.setAttribute("class", "row");
 	dishes.setAttribute("id", "dishes");
