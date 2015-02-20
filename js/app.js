40dbeed2058da1d7da021f763bf3914fcec580f2
francesco <info@francescovitale.com>
2015-02-20 20:24:54 +0100

    preparing the index.html

--- js/app.js
+++ js/app.js
@@ -12,16 +12,15 @@ $(function() {
 	window.stage = "starter";
 
 	//And create the needed controllers and views
-	var homeView = new HomeView($("#homeView"), model);
-	var totalMenuView = new TotalMenuView($("#totalMenuView"), model);
-	var totalMenuController =  new TotalMenuController(totalMenuView, model);
-	// var pickDishView = new PickDishView($("#pickDishView"), model);
-	// var pickDishController =  new PickDishController(pickDishView, model);
-	// var topBarView = new TopBarView($("#topBarView"), model);
-	var dishDetailView = new DishDetailView($("#dishDetail"), model);
-	var dishDetailController =  new DishDetailController(dishDetailView, model);
+	//var homeView = new HomeView($("#homeView"), model);
+	//var totalMenuView = new TotalMenuView($("#totalMenuView"), model);
+	//var totalMenuController =  new TotalMenuController(totalMenuView, model);
+	//var pickDishView = new PickDishView($("#pickDishView"), model);
+	//var pickDishController =  new PickDishController(pickDishView, model);
+	//var dishDetailView = new DishDetailView($("#dishDetail"), model);
+	//var dishDetailController =  new DishDetailController(dishDetailView, model);
 	// var confirmDinnerView = new ConfirmDinnerView($("#confirmDinnerView"), model);
-	// var recipeView = new RecipeView($("#recipeView"), model);
+	 var recipeView = new RecipeView($("#recipeView"), model);
 });
 
 
