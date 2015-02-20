7912a3ac946cd5c7948bd080ed75812e259bc509
francesco <info@francescovitale.com>
2015-02-20 18:28:18 +0100

    merged topBarView with other views

--- js/view/confirmDinnerView.js
+++ js/view/confirmDinnerView.js
@@ -15,6 +15,39 @@ var ConfirmDinnerView = function (container, model) {
 	var totalPrice = model.getTotalMenuPrice();
 	var guests = model.getNumberOfGuests();
 
+	// top bar
+	var topBar = document.createElement('div');
+	topBar.setAttribute("id", "top-bar");
+	topBar.setAttribute("class", "row amber lighten-3");
+
+	// My Dinner with number of guests
+	var dinnerLeft = document.createElement('div');
+	dinnerLeft.setAttribute("class", "col s12 m4 l4");
+	var dinnerHeading = document.createElement('h3');
+	dinnerHeading.setAttribute("class", "small-heading");
+	var dinnerHeadingText = document.createTextNode("My Dinner: " + guests + " people");
+	dinnerHeading.appendChild(dinnerHeadingText);
+	dinnerLeft.appendChild(dinnerHeading);
+
+	// back button
+	var dinnerRight = document.createElement('div');
+	dinnerRight.setAttribute("class", "col s12 m4 l4 offset-m4 offset-l4");
+
+	var buttonContainer = document.createElement('p');
+	var buttonLink = document.createElement('a');
+	buttonLink.setAttribute("class", "waves-effect waves-light btn");
+	var buttonIcon = document.createElement('i');
+	buttonIcon.setAttribute("class", "mdi-navigation-arrow-back left");
+	var buttonText = document.createTextNode("Go back and edit dinner");
+	buttonLink.appendChild(buttonIcon);
+	buttonLink.appendChild(buttonText);
+	buttonContainer.appendChild(buttonLink);
+	dinnerRight.appendChild(buttonContainer);
+
+	topBar.appendChild(dinnerLeft);
+	topBar.appendChild(dinnerRight);
+
+	// dishes div
 	var dishes = document.createElement('div');
 	dishes.setAttribute("class", "row");
 	dishes.setAttribute("id", "dishes");
@@ -81,6 +114,7 @@ var ConfirmDinnerView = function (container, model) {
 	dishes.appendChild(totalPriceDiv);
 
 	this.confirmDinner.html(dishes);
+	document.getElementById("confirmDinnerView").appendChild(topBar);
 	document.getElementById("confirmDinnerView").appendChild(dishes);
 }
 
